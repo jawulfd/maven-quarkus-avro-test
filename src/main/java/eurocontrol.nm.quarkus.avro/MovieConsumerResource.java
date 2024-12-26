49991ae9665d0dd0d09379cc2482548a3f12a76e
Garcia Ojero, Jesus <jgarciao@eservicios.indra.es>
2024-12-26 13:23:10 +0100

    initial commit

---
+++ src/main/java/eurocontrol.nm.quarkus.avro/MovieConsumerResource.java
@@ -0,0 +1,26 @@
+package eurocontrol.nm.quarkus.avro;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.reactive.RestStreamElementType;
+import dev.kameshs.kafka.quarkus.Movie;
+import io.smallrye.mutiny.Multi;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@ApplicationScoped
+@Path("/movies")
+public class MovieConsumerResource {
+
+  @Channel("movies-from-kafka")
+  Multi<Movie> movies;
+
+  @GET
+  @Produces(MediaType.SERVER_SENT_EVENTS)
+  @RestStreamElementType(MediaType.APPLICATION_JSON)
+  public Multi<Movie> stream() {
+    return movies;
+  }
+}
---
+++ src/main/java/eurocontrol.nm.quarkus.avro/MovieProducerResource.java
@@ -0,0 +1,26 @@
+package eurocontrol.nm.quarkus.avro;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
+import dev.kameshs.kafka.quarkus.Movie;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+@Path("/movies")
+public class MovieProducerResource {
+  private static final Logger LOGGER = Logger.getLogger(MovieProducerResource.class);
+
+  @Channel("movies-to-kafka")
+  Emitter<Movie> emitter;
+
+  @POST
+  @Path("/add")
+  public Response enqueueMovie(Movie movie) {
+    LOGGER.infof("Sending movie %s to Kafka", movie.getTitle());
+    emitter.send(movie);
+    return Response.accepted().build();
+  }
+
+}
